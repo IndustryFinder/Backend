5089f175ecbc0698df1a6506113d8c5ad511e898
arshia <arshiahosseinzadeh@yahoo.com>
2023-04-07 20:54:39 +0330

    CompanyShow feature test added.

---
+++ tests/Feature/UserManagement/CompanyShowTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Feature\UserManagement;
+
+use App\Models\Company;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CompanyShowTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+
+    /** @test */
+    public function GetCompanyDetailsSuccessfully()
+    {
+        $company = Company::factory(['is_active' => true])->create()->toArray();
+        $response = $this->postJson("/api/company/show/${company['id']}");
+        $response
+            ->assertStatus(200)
+            ->assertJsonPath('name',$company['name']);
+    }
+
+    /** @test */
+    public function UserCantGetDetailsOfInActiveCompany()
+    {
+        $company = Company::factory(['is_active' => false])->create()->toArray();
+        $response = $this->postJson("/api/company/show/${company['id']}");
+        $response->assertStatus(404);
+    }
+
+    /** @test */
+    public function UserCantGetDetailsOfCompanyWithWrongId()
+    {
+        $response = $this->postJson("/api/company/show/0");
+        $response->assertStatus(404);
+    }
+}
