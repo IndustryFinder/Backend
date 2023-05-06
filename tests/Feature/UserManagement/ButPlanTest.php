c8b7faea8b406ecda63829ba6ade94c740dd271c
arshia <arshiahosseinzadeh@yahoo.com>
2023-05-06 21:26:49 +0330

    new BuyPlan tests added to cover all types of payment plans

--- tests/Feature/UserManagement/ButPlanTest.php
+++ tests/Feature/UserManagement/ButPlanTest.php
@@ -40,4 +40,25 @@ class ButPlanTest extends TestCase
         $response->assertStatus(200);
         $this->assertDatabaseHas('users',['activePlan' => 'deluxe' , 'AdsRemaining' => 30]);
     }
+    /** @test  */
+    public function UserCanBuyMaxPlanSuccessfully(){
+        $user=User::factory(['wallet' => 100000])->create()->toArray();
+        $this->actingAs(User::find($user['id']));
+        $response=$this->getJson('/api/user/BuyPlan/4');
+        $response->assertStatus(200);
+        $this->assertDatabaseHas('users',['activePlan' => 'max' , 'AdsRemaining' => 45]);
+    }
+    /** @test  */
+    public function GuestCantBuyPlan(){
+        $user=User::factory(['wallet' => 25000])->create()->toArray();
+        $response=$this->getJson('/api/user/BuyPlan/1');
+        $response->assertStatus(401);
+    }
+    /** @test  */
+    public function UserCantBuyInvalidPlanId(){
+        $user=User::factory(['wallet' => 25000])->create()->toArray();
+        $this->actingAs(User::find($user['id']));
+        $response=$this->getJson('/api/user/BuyPlan/5');
+        $response->assertStatus(400);
+    }
 }
